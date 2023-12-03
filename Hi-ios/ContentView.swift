8339f4e2403f463b2f8f7fd3a74c5965e7f6476d
biiigwang <1246405757@qq.com>
2023-12-03 12:06:40 +0800

    Initial Commit

---
+++ Hi-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hi-ios
+//
+//  Created by biiigwang on 2023/12/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hi-ios/Hi_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hi_iosApp.swift
+//  Hi-ios
+//
+//  Created by biiigwang on 2023/12/3.
+//
+
+import SwiftUI
+
+@main
+struct Hi_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
