9e4dd4e8688cc3ea3fee5739d779fbbef088789b
biiigwang <1246405757@qq.com>
2023-12-05 22:44:47 +0800

    class3: Layout code and preview

--- Hi-ios/ContentView.swift
+++ Hi-ios/ContentView.swift
@@ -19,20 +19,28 @@ struct ContentView: View {
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            Text("20 sections - 3 hours")
+            Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
             Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
+                .frame(
+                    maxWidth:  .infinity ,
+                    alignment: .leading
+                )
         }
         .padding(.all, 20.0)
+        .padding(.vertical, 20.0)
         .frame(height: 350.0)
         .background(Color("Background"))
         .cornerRadius(30.0)
-        .shadow(radius: 10)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0.0, y: 10.0)
+        .padding(.horizontal, 20.0)
     }
 }
 
